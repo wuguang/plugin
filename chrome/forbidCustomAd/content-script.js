d544a1c71941f01ad3bac3264b144649ce5bc01c
wuguang <wuguang@wxblockchain.com>
2019-03-21 14:09:26 +0800

    update

---
+++ chrome/forbidCustomAd/content-script.js
@@ -0,0 +1,32 @@
+﻿class MyPage{
+    constructor(){
+        this.hostname = location.hostname;
+        this.init();
+    }
+
+    init(){
+        this.addEvent();
+    }
+
+    checkNode(){
+        if(this.hostname.indexOf('www.baidu.com')>-1){
+            let parentDom = this.$('container');
+            let rightContent = this.$('content_right');
+            if(parentDom && rightContent){
+                parentDom.removeChild(rightContent);
+            }
+        }
+    }
+
+    addEvent(){
+        document.addEventListener("DOMSubtreeModified", (e)=>{
+            this.checkNode();
+        });
+    }
+
+    $(id){
+        return document.getElementById(id);
+    }
+}
+
+new MyPage();
